ee1f5e11fa5861bfe48d5a9eb43676f02457d62e
AMRANE Amina <amiina.amr@gmail.com>
2022-03-07 12:51:04 +0100

    test a first simple graph in the main

--- main.scala
+++ main.scala
@@ -1,3 +1,18 @@
+package undirected
+
 object main extends App {
-    println("Hello World !")
+
+    println("Hello World !");
+
+    val vertices=Set("A","B","C");
+    val ed1=Edge[String]("A","B");
+    val ed2=Edge[String]("A","C");
+    println(ed2.adjacentTo("C"));
+    val edges=Set(ed1,ed2);
+    val sG=SimpleGraphDefaultImpl(vertices,edges);
+    println(sG.neighborsOf("C"));
+    println(sG.neighborsOf("A"));
+
+
+
 }
