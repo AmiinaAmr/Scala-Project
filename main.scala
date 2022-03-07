023cb3ac9a5bf0de06f28cdebab2614173c8be77
AMRANE Amina <amiina.amr@gmail.com>
2022-03-07 13:05:13 +0100

    commit amina

--- main.scala
+++ main.scala
@@ -3,7 +3,6 @@ package undirected
 object main extends App {
 
     println("Hello World !");
-
     val vertices=Set("A","B","C");
     val ed1=Edge[String]("A","B");
     val ed2=Edge[String]("A","C");
